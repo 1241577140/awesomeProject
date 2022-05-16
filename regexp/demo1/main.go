0ba3e4ed1568e23c1d00161c13b7cae04baa50e6
sunmeng <1241577140@qq.com>
2022-05-16 22:06:06 +0800

    [feat] changes  正则

--- regexp/demo1/main.go
+++ regexp/demo1/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
-	"runtime"
+	"strings"
 )
 
 func main() {
@@ -11,9 +12,19 @@ func main() {
 	if err != nil {
 		return
 	}
-	strings := compile.FindStringSubmatch("http://www.zhenai.com/zhenghun/aaa/bbb")
-	for _, s := range strings {
+	strs := compile.FindStringSubmatch("http://www.zhenai.com/zhenghun/aaa/bbb")
+	for _, s := range strs {
 		fmt.Println(s)
 	}
-	runtime.GC()
+
+	var str = "s1/s2/s3.json"
+	fmt.Println(strings.TrimPrefix(str, "/"))
+	var s = "1,2,3"
+	sl := make([]int, 0)
+	err = json.Unmarshal([]byte(s), &sl)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(s)
 }
